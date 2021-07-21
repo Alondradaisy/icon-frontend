b275aee2771cfb90ebfb82d1c36057c7e94c9524
Alondradaisy <alondra.warren96@gmail.com>
2021-07-21 08:07:10 -0700

    add handleUserLogout to PrivateRoute for user login/logout

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -3,15 +3,18 @@ import React, { Component } from "react";
 import { Route, Redirect } from "react-router-dom";
 import UserAuth from "../utils/UserAuth";
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+const PrivateRoute = ({ component: Component, handleUserLogout, ...rest }) => {
   return (
     <Route
       {...rest}
       render={(routerProps) =>
-        UserAuth() ? <Component {...routerProps} /> : <Redirect to="/login" />
+        UserAuth() ? (
+          <Component {...routerProps} handleUserLogout={handleUserLogout} />
+        ) : (
+          <Redirect to="/login" />
+        )
       }
     />
   );
 };
-
 export default PrivateRoute;
