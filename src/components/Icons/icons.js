e0e18d26595711e83644c2f4d2c83a6f5323c950
Alondradaisy <alondra.warren96@gmail.com>
2021-07-25 19:46:15 -0700

    delete questionnaire component from proj, add rce plate to icon.js

---
+++ src/components/Icons/icons.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import Axios from "../utils/Axios";
+
+export class Icons extends Component {
+  state = {};
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Icons;
